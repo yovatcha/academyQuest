7c510d5f65a19ee81fb366778016da90eef02943
Vatcharamai <yoyo.rodring@gmail.com>
2025-05-29 09:49:49 +0700

    fix lint

--- spec/requests/quests_spec.rb
+++ spec/requests/quests_spec.rb
@@ -13,7 +13,6 @@ require 'rails_helper'
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
 RSpec.describe "/quests", type: :request do
-  
   # This should return the minimal set of attributes required to create a valid
   # Quest. As you add validations to Quest, be sure to
   # adjust the attributes here as well.
